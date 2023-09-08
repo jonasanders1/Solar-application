1fef3a5b055a1c4bd2b07a0d67a50d8898171035
Jonas Andersen <94622735+jonasanders1@users.noreply.github.com>
2023-09-08 10:32:09 +0200

    Now up to date with app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-
-import { Text, View } from "react-native";
+import WelcomeRoutes from "./src/Routes/Welcome.Routes";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import Button from "./src/components/Button";
+import { View } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  // return <WelcomeRoutes />;
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello There!</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Button title" Icon={() => <></>} />
     </View>
   );
 }
